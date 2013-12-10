f30904770f6fcc88ee81964351612a42e8a3ac8a
Andreas Jacobsen <anjac@itu.dk>
2013-12-10 20:35:13 +0100

    fixing merging

--- AirplaneBooking/src/airplanebooking/AirplaneBooking.java
+++ AirplaneBooking/src/airplanebooking/AirplaneBooking.java
@@ -1,13 +1,5 @@
 package airplanebooking;
 
-<<<<<<< HEAD
-import airplanebooking.DB.Booking;
-import airplanebooking.DB.DatabaseHandler;
-import airplanebooking.DB.DatabaseInterface;
-import airplanebooking.DB.Flight;
-import airplanebooking.DB.Seat;
-=======
->>>>>>> 1d64a5b9750bab1fe6da3cf8567c2a7a584d2010
 import airplanebooking.swing.SwingMain;
 
 /**
